cd9d9e488c8646b536815254f835992b6debf192
Siarheimasiuk <winst73@gmail.com>
2022-09-05 00:46:51 +0300

    Change font text.

--- AlbomsFromThePhotosApp/Sources/Cell/CellFour.swift
+++ AlbomsFromThePhotosApp/Sources/Cell/CellFour.swift
@@ -23,6 +23,7 @@ class CellFour: UICollectionViewCell {
     private lazy var label: UILabel = {
         let label = UILabel()
         label.textColor = .systemBlue
+        label.font = .systemFont(ofSize: 20)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
